88bc15d11cc763b89167d0aa772ade9d9a55a676
Ryan Nixon <r.taiidani@gmail.com>
2022-05-28 10:34:13 -0700

    Move Hashi tooling to asdf

--- .chezmoiscripts/run_after_asdf.sh
+++ .chezmoiscripts/run_after_asdf.sh
@@ -8,4 +8,28 @@ if [[ ! $(asdf plugin list | grep '^golang$') ]]; then
     asdf plugin add golang
 fi
 
+if [[ ! $(asdf plugin list | grep '^terraform$') ]]; then
+    asdf plugin add terraform
+fi
+
+if [[ ! $(asdf plugin list | grep '^nomad$') ]]; then
+    asdf plugin add nomad
+fi
+
+if [[ ! $(asdf plugin list | grep '^consul$') ]]; then
+    asdf plugin add consul
+fi
+
+if [[ ! $(asdf plugin list | grep '^vault$') ]]; then
+    asdf plugin add vault
+fi
+
+if [[ ! $(asdf plugin list | grep '^waypoint$') ]]; then
+    asdf plugin add waypoint
+fi
+
+if [[ ! $(asdf plugin list | grep '^packer$') ]]; then
+    asdf plugin add packer
+fi
+
 asdf install
